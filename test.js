073ab0a823e3f846d224db5c7a048720dcf36347
Kal <58021911+kalyano@users.noreply.github.com>
2024-11-25 14:54:11 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
+const dom = new JSDOM(html);
+global.document = dom.window.document;
+
+require('./script');
+
+test('Button click displays text', () => {
+    const button = document.getElementById("displayTextButton");
+    button.click();
+    const text = document.getElementById("textContainer").innerText;
+    expect(text).toBe("Hello, CircleCI!");
+});
